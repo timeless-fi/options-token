db034ca48dd8b4d4bf028c75606fe2e3cb0991ac
ZeframLou <zeframlou@gmail.com>
2022-11-18 13:10:57 -0800

    feat: use CREATE3 deployment management

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,18 +1,18 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.13;
 
-import "forge-std/Script.sol";
-
 import {ERC20} from "solmate/tokens/ERC20.sol";
 
 import {OptionsToken} from "../src/OptionsToken.sol";
+import {CREATE3Script} from "./base/CREATE3Script.sol";
 import {BalancerOracle} from "../src/oracles/BalancerOracle.sol";
 import {IERC20Mintable} from "../src/interfaces/IERC20Mintable.sol";
 import {IBalancerTwapOracle} from "../src/interfaces/IBalancerTwapOracle.sol";
 
-contract DeployScript is Script {
+contract DeployScript is CREATE3Script("1.0.0") {
     function run() public returns (OptionsToken optionsToken, BalancerOracle oracle) {
         uint256 deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
+
         vm.startBroadcast(deployerPrivateKey);
 
         {
@@ -22,19 +22,35 @@ contract DeployScript is Script {
             uint56 secs = uint56(vm.envUint("ORACLE_SECS"));
             uint56 ago = uint56(vm.envUint("ORACLE_AGO"));
             uint128 minPrice = uint128(vm.envUint("ORACLE_MIN_PRICE"));
-            oracle = new BalancerOracle(balancerPriceOracle, owner, multiplier, secs, ago, minPrice);
+
+            oracle = BalancerOracle(
+                create3.deploy(
+                    getCreate3ContractSalt("BalancerOracle"),
+                    bytes.concat(
+                        type(BalancerOracle).creationCode,
+                        abi.encode(balancerPriceOracle, owner, multiplier, secs, ago, minPrice)
+                    )
+                )
+            );
         }
 
         {
             string memory name = vm.envString("OT_NAME");
             string memory symbol = vm.envString("OT_SYMBOL");
             address owner = vm.envAddress("OWNER");
-            address tokenAdmin = vm.envAddress("TOKEN_ADMIN");
+            address tokenAdmin = getCreate3Contract("TokenAdmin");
             ERC20 paymentToken = ERC20(vm.envAddress("OT_PAYMENT_TOKEN"));
-            IERC20Mintable underlyingToken = IERC20Mintable(vm.envAddress("OT_UNDERLYING_TOKEN"));
+            IERC20Mintable underlyingToken = IERC20Mintable(getCreate3Contract("TimelessToken"));
             address treasury = vm.envAddress("TREASURY");
-            optionsToken =
-                new OptionsToken(name, symbol, owner, tokenAdmin, paymentToken, underlyingToken, oracle, treasury);
+            bytes memory constructorParams =
+                abi.encode(name, symbol, owner, tokenAdmin, paymentToken, underlyingToken, oracle, treasury);
+
+            optionsToken = OptionsToken(
+                create3.deploy(
+                    getCreate3ContractSalt("OptionsToken"),
+                    bytes.concat(type(OptionsToken).creationCode, constructorParams)
+                )
+            );
         }
 
         vm.stopBroadcast();
